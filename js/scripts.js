4edd0a3119c4cde9e73d18a7e89c1c2ac3a1174c
Gavin Kimberlin <guviner@gmail.com>
2019-06-14 11:30:42 -0700

    begin writing javascript.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,14 @@
+function Beep(x) {
+  input.split(x)
+  console.log(x);
+};
+
+
+
 $(document).ready(function () {
   $(form#formInput).submit(function (event) {
     event.preventDefault();
+    var input = $('#userInput').val();
+    function(input);
   });
 });
