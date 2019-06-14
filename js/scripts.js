6dfe3f74c975c480ee8c38bfcc2672a478cf99f4
Gavin Kimberlin <guviner@gmail.com>
2019-06-14 09:37:24 -0700

    add html and javascript template.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,5 @@
-$(document).ready(function() {
-  $("#some-form").submit(function(event) {
+$(document).ready(function () {
+  $(form#formInput).submit(function (event) {
     event.preventDefault();
-
-    var someInputVar = $("#some-input").val(); // Don't forget it's a string without parseInt()
-    var someOutputVar = someInputVar;
-
-    $("#output").text(someOutputVar);
-    $("#output").show();
   });
 });
