8276b83d04fdfa5d6bee33bca833d881c91dc45f
Gavin Kimberlin <guviner@gmail.com>
2019-06-14 13:39:35 -0700

    get im sorry dave working.

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,35 @@
 $(document).ready(function () {
   $('form#formInput').submit(function (event) {
     event.preventDefault();
-    var input = $('#userInput').val();
+    var input = parseInt($('#userInput').val());
+    var intoNumber1 = parseInt(input);
+    var ans = [];
     function range() {
-      var ans = [];
-      for (var i = 0; i <= parseInt(input); i++) {
+      for (var i = 0; i <= intoNumber1; i++) {
         ans.push(i);
         console.log(ans);
       }
     };
+
+    var newAns = [];
     function beep() {
-      var split = input.split('');
-      for (var i = 0; i < split.length; i++) {
-        var toNumber = parseInt(split[i]);
-        console.log(toNumber);
-      }
+      for (var j = 0; j < ans.length; j++) {
+        var split = (ans[j].toString()).split('');
+        console.log(split);
+        var splitNumsArray = [];
+        for (var k = 0; k < split.length; k++) {
+          var toNumbers2 = parseInt(split[k]);
+          console.log(toNumbers2);
+          splitNumsArray.push(toNumbers2);
+          console.log(splitNumsArray);
+          if (splitNumsArray[k] === 3) {
+            ans.splice(j, 1, 'I\'m sorry, Dave. I\'m afraid I can\'t do that.')
+            console.log(ans);
+          };
+        };
+      };
     };
+
     range();
     beep();
   });
