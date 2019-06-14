33cd846283e06dc733b9c5f4c9ddc58ac08d49d5
Gavin Kimberlin <guviner@gmail.com>
2019-06-14 11:51:03 -0700

    get first spec working.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,22 @@
-function Beep(x) {
-  input.split(x)
-  console.log(x);
-};
-
-
-
 $(document).ready(function () {
-  $(form#formInput).submit(function (event) {
+  $('form#formInput').submit(function (event) {
     event.preventDefault();
     var input = $('#userInput').val();
-    function(input);
+    function range() {
+      var ans = [];
+      for (var i = 0; i <= parseInt(input); i++) {
+        ans.push(i);
+        console.log(ans);
+      }
+    };
+    function beep() {
+      var split = input.split('');
+      for (var i = 0; i < split.length; i++) {
+        var toNumber = parseInt(split[i]);
+        console.log(toNumber);
+      }
+    };
+    range();
+    beep();
   });
 });
