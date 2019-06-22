d8144d736d4a19c7693b8dda448418a9bf3314f4
Gavin Kimberlin <guviner@gmail.com>
2019-06-21 20:22:22 -0700

    separate business and user logic.

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,40 @@
-$(document).ready(function () {
-  $('form#formInput').submit(function (event) {
-    event.preventDefault();
-    var input = parseInt($('#userInput').val());
-    var intoNumber1 = parseInt(input);
-    var ans = [];
+function beep(input) {
 
-    function range() {
-      for (var i = 0; i <= intoNumber1; i++) {
-        ans.push(i);
-      }
-    };
+  var ans = [];
+  function range() {
+    for (var i = 0; i <= input; i++) {
+      ans.push(i);
+    }
+  };
 
-    var newAns = [];
-    function beep() {
-      for (var j = 0; j < ans.length; j++) {
-        var split = (ans[j].toString()).split('');
-        var splitNumsArray = [];
-        for (var k = 0; k < split.length; k++) {
-          var toNumbers2 = parseInt(split[k]);
-          splitNumsArray.push(toNumbers2);
-          if (splitNumsArray[k] === 3) {
-            ans.splice(j, 1, 'I\'m sorry, Dave. I\'m afraid I can\'t do that.');
-          }else if (splitNumsArray[k] === 2) {
-            ans.splice(j, 1, 'Boop');
-          }else if (splitNumsArray[k] === 1) {
-            ans.splice(j, 1, 'Beep');
-          }
-        };
+  function boop() {
+    for (var j = 0; j < ans.length; j++) {
+      var split = (ans[j].toString()).split('');
+      var splitNumsArray = [];
+      for (var k = 0; k < split.length; k++) {
+        var toNumbers2 = parseInt(split[k]);
+        splitNumsArray.push(toNumbers2);
+        if (splitNumsArray[k] === 3) {
+          ans.splice(j, 1, 'I\'m sorry, Dave. I\'m afraid I can\'t do that.');
+        }else if (splitNumsArray[k] === 2) {
+          ans.splice(j, 1, 'Boop');
+        }else if (splitNumsArray[k] === 1) {
+          ans.splice(j, 1, 'Beep');
+        }
       };
     };
+  };
+  range();
+  boop();
+  return (ans);
+};
 
-    range();
-    beep();
-    console.log(ans);
-    ($('h4.output')).text(ans);
+
+$(document).ready(function () {
+  $('form#formInput').submit(function (event) {
+    var userInput = parseInt($('#userInput').val());
+    event.preventDefault();
+    var result = beep(userInput);
+    ($('h4.output')).text(result);
   });
 });
